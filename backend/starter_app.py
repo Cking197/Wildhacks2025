29c349d097ae91e55c79278ca8259f05ec7a7f59
Cking197 <114192878+Cking197@users.noreply.github.com>
2025-04-05 13:50:32 -0500

    added mongo server connection

--- backend/starter_app.py
+++ backend/starter_app.py
@@ -5,7 +5,16 @@ from pymongo import MongoClient
 app = Flask(__name__)
 CORS(app)
 
-client = MongoClient("mongodb://localhost:27017/")
+uri = "mongodb+srv://admin:NUAdmin4321@cluster0.yospykr.mongodb.net/?appName=Cluster0"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 db = client["mydatabase"]
 collection = db["messages"]
 
