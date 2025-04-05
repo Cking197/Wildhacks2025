2c08562227d7db0d1c02211a598b8875f9096d6c
Eiko Reisz <eiko.reisz@gmail.com>
2025-04-05 15:32:32 -0500

    more function changes v1

--- backend/starter_app.py
+++ backend/starter_app.py
@@ -24,49 +24,58 @@ users = db["users"]
 def create_user():
     try:
         data = request.get_json()
-        users.insert_one(data)
-        return jsonify(message="User created!")
+        insertid = users.insert_one(data)
+        return jsonify(message="User created!", _id=str(result.inserted_id))
     except Exception as e:
         print("Error: ", e)
-        return jsonify(message="Error: User not created")
+        return jsonify(message="Error: User not created"), 500
 
 @app.route("/updateUser", methods=["POST"])
 def update_user():
     try:
         data = request.get_json()
-        users.updateOne(
-            {"_id": data["_id"]["$oid"]},
-            {"$set": {
-                "name": data["name"],
-                "pastHobbies": data["pastHobbies"],
-                "activeHobbies": data["activeHobbies"],
-                "location": data["location"],
-                "availability": data["availability"]
-            }})
+        user_id = ObjectId(data["_id"])  # assuming frontend sends _id as a string
+        update_fields = {
+            "name": data["name"],
+            "pastHobbies": data["pastHobbies"],
+            "activeHobbies": data["activeHobbies"],
+            "location": data["location"],
+            "availability": data["availability"]
+        }
+        users.updateOne({"_id": data["_id"]["$oid"]}, {"$set": update_fields})
         return jsonify(message="User updated!")
     except Exception as e:
         print("Error: ", e)
-        return jsonify(message="Error: User not updated")
+        return jsonify(message="Error: User not updated"), 500
 
 @app.route("/getUser", methods=["GET"])
 def get_user():
     try:
         data = request.get_json()
-        user = users.findOne({"_id": data["_id"]["$oid"]}, {}, {})
-        return jsonify(user)
+        user_id = ObjectId(data["_id"])
+        user = users.find_one({"_id": user_id})
+        if user:
+            user["_id"] = str(user["_id"])  # convert ObjectId to string for JSON
+            return jsonify(user)
+        else:
+            return jsonify(message="User not found"), 404
     except Exception as e:
-        print("Error: ", e)
-        return jsonify(message="Error: User not found")
+        print("Error:", e)
+        return jsonify(message="Error: User not found"), 500
 
 @app.route("/deleteUser", methods=["GET"])
 def delete_user():
     try:
         data = request.get_json()
-        user = users.deleteOne({"_id": data["_id"]["$oid"]}, {}, {})
-        return jsonify(user)
+        user_id = ObjectId(data["_id"])
+        result = users.delete_one({"_id": user_id})
+        if result.deleted_count == 1:
+            return jsonify(message="User deleted!")
+        else:
+            return jsonify(message="User not found"), 404
     except Exception as e:
-        print("Error: ", e)
-        return jsonify(message="Error: User not deleted")
+        print("Error:", e)
+        return jsonify(message="Error: User not deleted"), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
