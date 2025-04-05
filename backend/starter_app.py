66b9ca423befce3f33fe15236b84b84e1d5dab53
Eiko Reisz <eiko.reisz@gmail.com>
2025-04-05 16:00:16 -0500

    env file

--- backend/starter_app.py
+++ backend/starter_app.py
@@ -4,11 +4,16 @@ from bson import ObjectId
 from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 CORS(app)
 
-uri = "hi"
+load_dotenv()
+uri = os.getenv("MONGODB_URI")
+
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
