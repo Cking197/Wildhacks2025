6753092ea7e2a0c0b1ce15a2db29b9d3f6591087
Eiko Reisz <eiko.reisz@gmail.com>
2025-04-05 19:12:40 -0500

    gemini functions

--- backend/src/app.py
+++ backend/src/app.py
@@ -74,11 +74,11 @@ def get_user():
         print("Error:", e)
         return jsonify(message="Error: User not found"), 500
 
-@app.route("/deleteUser", methods=["GET"])
+@app.route("/deleteUser", methods=["DELETE"])
 def delete_user():
     try:
         data = request.get_json()
-        user_id = ObjectId(data["_id"])
+        user_id = ObjectId(data["_id"]["$oid"])
         result = users.delete_one({"_id": user_id})
         if result.deleted_count == 1:
             return jsonify(message="User deleted!")
@@ -88,13 +88,45 @@ def delete_user():
         print("Error:", e)
         return jsonify(message="Error: User not deleted"), 500
 
-"""IN PROGRESS: TODO
-response = geminiClient.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
-)
+@app.route("/createHobbies", methods=["POST"])
+def create_Hobbies():
+    try:
+        data = request.get_json()
+        locations = ["urban", "suburban", "rural"]
+        availability = ["0-3", "3-6", "6+"]
 
-print(response.text)
-"""
+        hobbies = ', '.join([obj["activity"] for obj in data["pastHobbies"]])
+        
+        prompt = f"""As someone who has previously spent time {hobbies}, please give me a JSON formatted list of 3 related or similar hobbies that work for
+            someone who is {data["age"]} years old, lives in a {locations[data["location"]]} area, is willing to spend ${data["budget"]}, and is willing
+            to invest {availability[data["availability"]]} hours per week on these hobbies.
+        """
+
+        response = geminiClient.models.generate_content(model="gemini-2.0-flash", contents=prompt)
+        print(response.text)
+        return jsonify(response.text)
+    except:
+        print("Error:", e)
+        return jsonify(message="Error: Unable to create hobbies"), 500
+    
+@app.route("/createTasks", methods=["POST"])
+def create_Hobbies():
+    try:
+        data = request.get_json()
+        hobby = data["hobby"]
+        
+        prompt = f"""I'm really interested in {hobby["activity"]}, but have no idea where to start. Please create a JSON formatted list of at least
+            2 tasks to help me figure out where or how to start given my time constraint of {hobby["time"]} weekly hours and cost constraint of 
+            ${hobby["cost"]}.
+        """
+
+        response = geminiClient.models.generate_content(model="gemini-2.0-flash", contents=prompt)
+        print(response.text)
+        return jsonify(response.txt)
+    except:
+        print("Error:", e)
+        return jsonify(message="Error: Unable to create tasks"), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
